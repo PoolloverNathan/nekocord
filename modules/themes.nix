00ef3fe040c72724dc42654da6540aa189d188c5
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-11-02 15:13:40 -0400

    feat: themes

--- modules/themes.nix
+++ modules/themes.nix
@@ -1,8 +1,31 @@
-{ pkgs, lib, ... }:
 {
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  options.themes = lib.mkOption {
+    description = ''
+      A list of themes to install.
+    '';
+    type =
+      with lib.types;
+      import ../namedAttrsOf.nix lib (name: coercedTo lines (builtins.toFile "${name}.css") path);
+    default = { };
+  };
   options.build.themesDir = lib.mkOption {
     internal = true;
     type = lib.types.pathInStore;
   };
-  config.build.themesDir = pkgs.runCommand "themes" { } "mkdir $out";
+  config.build.themesDir = pkgs.runCommand "themes" { } ''
+    mkdir $out
+    ${lib.concatLines (
+      lib.mapAttrsToList (
+        name: path: # bash
+        ''
+          ln -vs ${path} $out/${lib.escapeShellArg name}.css
+        '') config.themes
+    )}
+  '';
 }
